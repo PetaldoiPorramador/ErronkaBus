db245220ba3fab1bb3bd68c86adf7a845624e157
Txanrru <oier1310@gmail.com>
2021-12-22 11:40:24 +0100

    Imports borrados

--- busak/dao/DAOBilete.java
+++ busak/dao/DAOBilete.java
@@ -1,13 +1,10 @@
 package busak.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
-import java.time.LocalTime;
-
 import busak.objektuak.Bilete;
 
 public class DAOBilete {
