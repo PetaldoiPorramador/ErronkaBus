09e6c4eaa4796949c485bc1068ce8f53ed51004b
unaiduran6 <gusudh@gmail.com>
2022-01-10 10:54:57 +0100

    erabiltzaile cambio

--- busak/objektuak/Erabiltzaile.java
+++ busak/objektuak/Erabiltzaile.java
@@ -1,47 +1,59 @@
 package busak.objektuak;
 
 public class Erabiltzaile {
+    private String nanAiz, izenAbizenak, pasahitza;
+    private ArrayList<Bilete> bileteak;
+    private Scanner teklatua = new Scanner(System.in);
 
-    private String nan;
-    private String izenAbizenak;
-    private String pasahitza;
-
-    public Erabiltzaile(String nan, String izenAbizenak, String pasahitza) {
-        this.nan = nan;
-        this.izenAbizenak = izenAbizenak;
-        this.pasahitza = pasahitza;
+    public Erabiltzaile(String EnanAiz, String EizenAbizenak, String Epasahitza) {
+        this.nanAiz = EnanAiz;
+        this.izenAbizenak = EizenAbizenak;
+        this.pasahitza = Epasahitza;
+        bileteak = new ArrayList<Bilete>();
     }
 
-    public Erabiltzaile() {
-
-        this.nan = null;
-        this.izenAbizenak = null;
-        this.pasahitza = null;
-
+    public void erosiBilete(int kode, float ordaintzekoa, Timestamp hasData, Timestamp bukData, Geltoki hasGeltoki,
+            Geltoki bukGeltoki) {
+        Bilete bileteBerria = new Bilete(kode, ordaintzekoa, hasData, bukData, hasGeltoki, bukGeltoki);
+        bileteak.add(bileteBerria);
     }
 
-    public String getNan() {
-        return nan;
+    public void kantzelatuBilete(int num) {
+        // lehenengo eta behin, erabiltzailearen bileteak erakutsi, gero nahi duena
+        // ezabatu
+        ikusiBileteak();
+        bileteak.remove(num);
     }
 
-    public void setNan(String nan) {
-        this.nan = nan;
+    public void ikusiBileteak() {
+        // erakutsi erabiltzailearen bilete guztiak
+        for (int i = 0; i < bileteak.size(); i++) {
+            // TODO: con todos los getter
+            System.out.println(i + ": " + bileteak.get(i));
+        }
+    }
+
+    public void setNanAiz(nan) {
+        this.nanAiz = nan;
+    }
+
+    public void setIzenAbizenak(izenAbiz) {
+        this.izenAbizenak = izenAbiz;
+    }
+
+    public void setPasahitza(pass) {
+        this.pasahitza = pass;
+    }
+
+    public String getNanAiz() {
+        return nanAiz;
     }
 
     public String getIzenAbizenak() {
         return izenAbizenak;
     }
 
-    public void setIzenAbizenak(String izenAbizenak) {
-        this.izenAbizenak = izenAbizenak;
-    }
-
     public String getPasahitza() {
         return pasahitza;
     }
-
-    public void setPasahitza(String pasahitza) {
-        this.pasahitza = pasahitza;
-    }
-
 }
